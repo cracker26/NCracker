65e1152edbc4590c25eb56541f098f9c11aaddd4
ApaNC <44711841+ApaNC@users.noreply.github.com>
2018-12-03 20:02:50 +0400

    Add files via upload

--- cli/src/main/java/com/team26/Main.java
+++ cli/src/main/java/com/team26/Main.java
@@ -1,7 +1,57 @@
 package com.team26;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hi");
+import java.util.Scanner;
+import java.text.DecimalFormat;
+import java.io.IOException;
+
+public class Main
+{
+    void menu()
+    {
+        try
+        {
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Choose option, 1-3"
+                + "\n\t1. Browse tracks" + "\n\t2. Browse Genre" + "\n\t3. Cancel");
+        byte ch = kb.nextByte();
+
+        if (ch == 1)
+            Tracks();
+        else if (ch == 2)
+            Genre();
+        else if (ch == 3)
+            System.exit(0);
+        else if (ch >= 0 || ch > 3)
+        {
+            System.out.println("Invalid input");
+            menu();
+        }
+
+    }
+
+    catch(Exception e)
+    {
+        System.out.println("Invalid input");
+        menu();
+
+
+    }
+}
+
+void Tracks()
+{
+
+}
+
+void Genre()
+{
+
+}
+
+
+    public static void main(String[] args)
+    {
+    Main f = new Main ();
+    f.menu();
     }
 }
